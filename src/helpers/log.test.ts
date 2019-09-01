0ef92b19e28936394b43ad227d5aba27fb86ccd2
Ryan Poe <baublet@gmail.com>
2019-09-01 12:48:00 -0500

    Add logger tests

--- src/helpers/log.test.ts
+++ src/helpers/log.test.ts
@@ -1,5 +1,19 @@
 import log from "./log";
+const mockFn = jest.fn();
+const originalConsole = console;
+
+beforeEach(() => {
+  console = originalConsole;
+});
 
 it("logs properly and maintains the context", () => {
-  log("test!", { toots: 123 })
-})
+  console.log = mockFn;
+  log("test!", { toots: 123 });
+  expect(console.log).toHaveBeenCalled();
+  expect(console.log).toHaveBeenCalledWith(
+    "/src/helpers/log.test.ts:11:6",
+    "\n\n",
+    "test!",
+    `{\n\ttoots: 123\n}`
+  );
+});
