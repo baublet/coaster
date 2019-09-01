6f8add685566d30bca500ed296e6c0c5da4b1ac5
Ryan Poe <baublet@gmail.com>
2019-09-01 12:03:14 -0500

    Remove unused type

--- src/model/error/noPersistAdapterError.ts
+++ src/model/error/noPersistAdapterError.ts
@@ -1,5 +1,3 @@
-import { Model } from "model/createModel";
-
 export default function noPersistAdapterError(modelName: string) {
   return `You tried to call a model's persist functionality on the model ${modelName}, which was never passed a persist layer. Fix this error by passing "persistsWith" as part of your model declaration.`;
 }
