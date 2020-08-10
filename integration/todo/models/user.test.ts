3f903a1f00bf87d8fa521545f1fee63adcbe1446
Ryan Poe <baublet@gmail.com>
2020-08-10 11:20:33 -0500

    Remove integration testS

--- integration/todo/models/user.test.ts
+++
@@ -1,7 +0,0 @@
-// import User from "./user";
-// import Todo from "./todo"
-it("can persist a user", async () => {
-  // const user = User({
-  //   name: "Jaime"
-  // });
-});
--- integration/todo/persist.ts
+++
@@ -1,8 +0,0 @@
-import { connect } from "persist";
-
-export const database = connect({
-  client: "sqlite3",
-  connection: {
-    filename: ":memory"
-  }
-});
