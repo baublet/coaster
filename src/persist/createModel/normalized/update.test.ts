94f81e97cdfd2a50a3ff792250b8bc35f4fe3f8c
Ryan Poe <baublet@gmail.com>
2020-08-20 15:11:47 -0500

    Update

---
+++ src/persist/createModel/normalized/update.test.ts
@@ -0,0 +1,90 @@
+import { createSchema, Schema, SchemaNodeType } from "schema";
+
+import { createTablesFromSchema } from "persist/helpers/createTablesFromSchema";
+import { createTestConnection } from "persist/helpers/createTestConnection";
+
+import { createUpdateFunction } from "./update";
+
+const schema: Schema = createSchema({
+  entities: [
+    {
+      name: "TestObject",
+      nodes: {
+        id: SchemaNodeType.NUMBER,
+        name: SchemaNodeType.STRING,
+        active: SchemaNodeType.BOOLEAN,
+      },
+    },
+  ],
+});
+
+interface TestObject {
+  id: number;
+  name: string;
+  active: boolean;
+}
+
+it("creates and persists an entity: partial model", async () => {
+  const connection = await createTestConnection();
+  const [tables] = await createTablesFromSchema(connection, schema);
+  const update = createUpdateFunction<TestObject, TestObject>({
+    schema,
+    connection,
+    entity: "TestObject",
+    tableName: tables["TestObject"],
+  });
+
+  await connection
+    .insert({ name: "Object 1", active: false })
+    .into(tables["TestObject"]);
+  await connection
+    .insert({ name: "Object 2", active: false })
+    .into(tables["TestObject"]);
+  await connection
+    .insert({ name: "Object 3", active: true })
+    .into(tables["TestObject"]);
+
+  const [objectToChange] = await connection
+    .select("*")
+    .from(tables["TestObject"])
+    .where({ name: "Object 1" })
+    .limit(1);
+
+  objectToChange.active = 1;
+  objectToChange.name = "Changed Object Name";
+  const updatedObject = await update(objectToChange);
+
+  expect(updatedObject).toEqual(objectToChange);
+});
+
+it("creates and persists an entity: partial model", async () => {
+  const connection = await createTestConnection();
+  const [tables] = await createTablesFromSchema(connection, schema);
+  const update = createUpdateFunction<TestObject, TestObject>({
+    schema,
+    connection,
+    entity: "TestObject",
+    tableName: tables["TestObject"],
+  });
+
+  await connection
+    .insert({ name: "Object 1", active: false })
+    .into(tables["TestObject"]);
+  await connection
+    .insert({ name: "Object 2", active: false })
+    .into(tables["TestObject"]);
+  await connection
+    .insert({ name: "Object 3", active: true })
+    .into(tables["TestObject"]);
+
+  const updatedObject = await update(1, { name: "Changed Object" });
+  const [row] = await connection
+    .select("*")
+    .from(tables["TestObject"])
+    .where({ id: 1 })
+    .limit(1);
+
+  expect(updatedObject).toEqual(row);
+  expect(updatedObject.name).toEqual("Changed Object");
+  expect(row.name).toEqual("Changed Object");
+});
---
+++ src/persist/createModel/normalized/update.ts
@@ -0,0 +1,75 @@
+import { NormalizedModelFactory, Model, NormalizedModel } from "../createModel";
+import { Connection } from "persist/connection";
+
+import { CreateModelFactoryFullArguments } from "../createModel";
+import { getUniqueIdFieldForEntityInSchema } from "persist/helpers/getUniqueIdFieldForEntityInSchema";
+
+async function updateAndReturn<M extends Model, NM extends NormalizedModel>(
+  connection: Connection,
+  tableName: string,
+  uniqueIdField: string,
+  id: string | number,
+  data: Partial<NM>
+): Promise<NM> {
+  await connection
+    .table(tableName)
+    .update(data)
+    .where({ [uniqueIdField]: id })
+    .limit(1);
+  const fetchedResults = await connection
+    .table(tableName)
+    .select("*")
+    .where({ [uniqueIdField]: id })
+    .limit(1);
+  if (fetchedResults.length === 1) {
+    return fetchedResults[0] as NM;
+  }
+  throw new Error(
+    `Unable to find updated ID ${id} on table ${tableName} by its unique ID field, ${uniqueIdField}`
+  );
+}
+
+export function createUpdateFunction<
+  M extends Model,
+  NM extends NormalizedModel
+>({
+  schema,
+  entity,
+  connection,
+  tableName,
+}: CreateModelFactoryFullArguments): NormalizedModelFactory<M, NM>["update"] {
+  const uniqueIdField = getUniqueIdFieldForEntityInSchema(schema, entity);
+  async function update(
+    modelOrId: Partial<NM> | string | number,
+    maybeData?: Partial<NM>
+  ): Promise<NM | NM[]> {
+    if (typeof modelOrId === "object") {
+      const id = modelOrId[uniqueIdField];
+      if (!id) {
+        throw new Error(
+          `You cannot update a model or partial model without giving it a unique ID field. Expected the model to have a field called ${uniqueIdField}. Instead we got the keys: ${Object.keys(
+            modelOrId
+          )}`
+        );
+      }
+      const data = { ...modelOrId };
+      if (typeof data[uniqueIdField] !== "undefined") {
+        delete data[uniqueIdField];
+      }
+      return updateAndReturn(connection, tableName, uniqueIdField, id, data);
+    }
+    const data = { ...maybeData };
+    if (typeof data[uniqueIdField] !== "undefined") {
+      delete data[uniqueIdField];
+    }
+    return updateAndReturn<M, NM>(
+      connection,
+      tableName,
+      uniqueIdField,
+      modelOrId,
+      data
+    );
+  }
+
+  return update as NormalizedModelFactory<M, NM>["update"];
+}
