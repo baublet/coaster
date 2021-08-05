a35f0011d81fdcfe4a957b95b45b2613e543f23f
Ryan Poe <baublet@gmail.com>
2021-08-05 09:58:44 -0500

    Minor improvements

--- src/generateNames/generateNames.ts
+++ src/generateNames/generateNames.ts
@@ -1,21 +1,29 @@
-export function generateNames(
+import { camelCase, pascalCase } from "change-case";
+import pluralize from "pluralize";
+import memoize from "lodash.memoize";
+
+const unmemoizedGenerateNames = (
   name: string
 ): {
+  pluralCamel: string;
+  pluralPascal: string;
   raw: string;
   rawCamel: string;
   rawPascal: string;
   singularCamel: string;
   singularPascal: string;
-  pluralCamel: string;
-  pluralPascal: string;
-} {
+} => {
   return {
+    pluralCamel: camelCase(pluralize.plural(name)),
+    pluralPascal: pascalCase(pluralize.plural(name)),
     raw: name,
-    rawCamel: name,
-    rawPascal: name,
-    singularCamel: name,
-    pluralCamel: name,
-    pluralPascal: name,
-    singularPascal: name,
+    rawCamel: camelCase(name),
+    rawPascal: pascalCase(name),
+    singularCamel: camelCase(pluralize.singular(name)),
+    singularPascal: pascalCase(pluralize.singular(name)),
   };
-}
+};
+
+export const generateNames: typeof unmemoizedGenerateNames = memoize(
+  unmemoizedGenerateNames
+);
