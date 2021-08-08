76f2a3246f3faa7b5f33b7f831332c946ac8fb0e
Ryan Poe <baublet@gmail.com>
2021-08-08 17:45:48 -0500

    Fix mock schema

--- src/generateORM/mockRawSchema.ts
+++ src/generateORM/mockRawSchema.ts
@@ -13,7 +13,7 @@ export const getMockRawSchema: () => RawSchema = () => ({
           name: "id",
           columnType: "columnType",
           nullable: false,
-          type: "unknown",
+          type: "string",
           foreignKeys: [],
           uniqueConstraints: [],
         },
