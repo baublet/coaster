1b63e016f174b719dca10d9d6e63a623276709c5
Ryan Poe <baublet@gmail.com>
2022-06-05 11:35:59 -0500

    fixes

---
+++ src/utils/src/fullyResolve.test.ts
@@ -0,0 +1,17 @@
+import { expect, it } from "@baublet/coaster-unit-test";
+
+import { fullyResolve } from "./fullyResolve";
+
+it.each([
+  [
+    () => 123,
+    123,
+    Promise.resolve(123),
+    Promise.resolve(() => 123),
+    () => Promise.resolve(123),
+    () => () => Promise.resolve(123),
+    () => () => 123,
+  ],
+])("returns 123 from subject", async (subject) => {
+  await expect(fullyResolve(subject)).resolves.toEqual(123);
+});
