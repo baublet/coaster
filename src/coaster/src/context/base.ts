e61fcd52faa9b617f7124309cf5f68cf58db13f9
Ryan Poe <baublet@gmail.com>
2022-08-06 07:56:21 -0500

    Minor updates

--- src/coaster/src/context/base.ts
+++ src/coaster/src/context/base.ts
@@ -7,3 +7,12 @@ export interface Context {
   setLogService: (logService: LogService) => void;
   getLogService: () => LogService;
 }
+
+/**
+ * For core internals that need to be exposed to plugins and deep internals,
+ * yet configurable by users, we need a known service map to let users store
+ * and modify the services they wish us (and plugins) to use.
+ */
+export interface KnownServiceMap {
+  log: LogService;
+}
--- src/coaster/src/context/getContextLogProperties.ts
+++ src/coaster/src/context/getContextLogProperties.ts
@@ -3,10 +3,16 @@ import { ServiceContainer } from "@baublet/service-container";
 
 import { Context } from "./base";
 
+/**
+ * Logging is core functionality that needs to be shared by any plugins
+ * and other things outside of userspace, yet necessarily configurable by
+ * developers. We thus have to expose the logging service from the context
+ * for ease of access.
+ */
 export function getContextLogProperties(
   serviceContainer: ServiceContainer
 ): Pick<Context, "log" | "setLogService" | "getLogService"> {
-  let logService: LogService = () => console;
+  let logService: ServiceContainer<LogService> = () => console;
 
   return {
     setLogService: (newLogService) => {
