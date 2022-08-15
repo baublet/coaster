baa4a0657eca4b91f4802b1eecba152b38ae4a6d
Ryan Poe <baublet@gmail.com>
2022-08-15 07:40:08 -0500

    Fixes

--- src/coaster/src/graphql/createGraphqlTrack.ts
+++ src/coaster/src/graphql/createGraphqlTrack.ts
@@ -104,6 +104,7 @@ export async function createGraphqlTrack({
   });
 
   return {
+    __isCoasterTrack: true,
     build: () => {},
     handler: graphqlHandler,
   };
--- src/coaster/src/track/types.ts
+++ src/coaster/src/track/types.ts
@@ -3,6 +3,7 @@ import { CoasterError, TypeOrPromiseType } from "@baublet/coaster-utils";
 import { EndpointHandler } from "../endpoints/types";
 
 export interface CoasterTrack {
+  __isCoasterTrack: true;
   build: () => TypeOrPromiseType<undefined | CoasterError | void>;
   watchPaths?: string[];
   handler: EndpointHandler;
